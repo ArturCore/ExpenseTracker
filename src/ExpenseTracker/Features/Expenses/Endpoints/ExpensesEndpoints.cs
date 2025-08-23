eb2394a9b9111e890981dc397d29dfe9acfc2dba
ArturCode <a.khlapuk@bankvostok.com.ua>
2025-08-23 20:22:20 +0200

    Add basic endpoints

---
+++ src/ExpenseTracker/Features/Expenses/Endpoints/ExpensesEndpoints.cs
@@ -0,0 +1,17 @@
+﻿namespace ExpenseTracker.Api.Features.Expenses.Endpoints
+{
+    public static class ExpensesEndpoints
+    {
+        public static IEndpointRouteBuilder MapExpenseEndpoints(this IEndpointRouteBuilder app)
+        {
+            var expenses = app.MapGroup("/api/expenses").WithTags("Expenses");
+
+            expenses.MapGet("/", () => Results.Ok(new { Id = 1, Amount = 100500, Description = "Test"}));
+            expenses.MapPost("/", () => "Hello");
+            expenses.MapPut("/", () => "Hello");
+            expenses.MapDelete("/", () => "Hello");
+
+            return app;
+        }
+    }
+}
--- src/ExpenseTracker/Program.cs
+++ src/ExpenseTracker/Program.cs
@@ -1,9 +1,13 @@
+using ExpenseTracker.Api.Features.Expenses.Endpoints;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.MapExpenseEndpoints();
 
 app.UseHttpsRedirection();
 
