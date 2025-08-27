1790cd49d4f8f9a94d5836e0ea720264bff76da7
ArturCode <a.khlapuk@bankvostok.com.ua>
2025-08-27 23:27:53 +0200

    Add expense repository

---
+++ src/ExpenseTracker/Features/Expenses/Repository/IExpenseRepository.cs
@@ -0,0 +1,13 @@
+﻿using ExpenseTracker.Api.Features.Expenses.Models;
+
+namespace ExpenseTracker.Api.Features.Expenses.Repository
+{
+    public interface IExpenseRepository
+    {
+        Task<Expense?> GetByIdAsync(Guid id);
+        Task<List<Expense?>> GetAllAsync();
+        Task AddAsync(Expense expense);
+        Task UpdateAsync(Expense expense);
+        Task DeleteAsync(Guid id);
+    }
+}
